3a57e548fbd73bb7a47f325a135dce2a1cc47ef0
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2022-01-02 18:32:39 +0800

    update images and necessary things

---
+++ lib/c_color.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const MaterialColor primaryColor =
+      MaterialColor(0xFF5843be, <int, Color>{
+    50: Color(0xFF4f3cab), // 10%
+    100: Color(0xFF463698), //20%
+    200: Color(0xFF3e2f85), //30%
+    300: Color(0xFF352872), //40%
+    400: Color(0xFF2c225f), //50%
+    500: Color(0xFF231b4c), //60%
+    600: Color(0xFF1a1439), //70%
+    700: Color(0xFF120d26), //80%
+    800: Color(0xFF090713), //90%
+    900: Color(0xFF000000), //100%
+  });
+
+  static const MaterialColor secondaryColor =
+      MaterialColor(0xFFFF9376, <int, Color>{
+    50: Color(0xFFe6846a), // 10%
+    100: Color(0xFFcc765e), //20%
+    200: Color(0xFFb36753), //30%
+    300: Color(0xFF995847), //40%
+    400: Color(0xFF804a3b), //50%
+    500: Color(0xFF663b2f), //60%
+    600: Color(0xFF4c2c23), //70%
+    700: Color(0xFF331d18), //80%
+    800: Color(0xFF190f0c), //90%
+    900: Color(0xFF000000), //100%
+  });
+
+  static const MaterialColor greyColor = MaterialColor(0xFFa9b0a6, <int, Color>{
+    50: Color(0xFF989e95), // 10%
+    100: Color(0xFF878d85), //20%
+    200: Color(0xFF767b74), //30%
+    300: Color(0xFF656a64), //40%
+    400: Color(0xFF555853), //50%
+    500: Color(0xFF444642), //60%
+    600: Color(0xFF333532), //70%
+    700: Color(0xFF222321), //80%
+    800: Color(0xFF111211), //90%
+    900: Color(0xFF000000), //100%
+  });
+  // static const MaterialColor bgColor = MaterialColor(0xFFe5e5e5, <int, Color>{
+  //   50: Color(0xFFcecece), // 10%
+  //   100: Color(0xFFb7b7b7), //20%
+  //   200: Color(0xFFa0a0a0), //30%
+  //   300: Color(0xFF898989), //40%
+  //   400: Color(0xFF737373), //50%
+  //   500: Color(0xFF5c5c5c), //60%
+  //   600: Color(0xFF454545), //70%
+  //   700: Color(0xFF2e2e2e), //80%
+  //   800: Color(0xFF171717), //90%
+  //   900: Color(0xFF000000), //100%
+  // });
+
+  static const MaterialColor whiteColor =
+      MaterialColor(0xFFFFFFFF, <int, Color>{
+    50: Color(0xFFcecece), // 10%
+    100: Color(0xFFb7b7b7), //20%
+    200: Color(0xFFa0a0a0), //30%
+    300: Color(0xFF898989), //40%
+    400: Color(0xFF737373), //50%
+    500: Color(0xFF5c5c5c), //60%
+    600: Color(0xFF454545), //70%
+    700: Color(0xFF2e2e2e), //80%
+    800: Color(0xFF171717), //90%
+    900: Color(0xFF000000), //100%
+  });
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pppp/main.dart';
+import 'package:ppp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
