a26d2ba3415eb012923f90cee74c36f9ac867afc
Santa Renoir Greyhat <84732428+Santa-wijaya@users.noreply.github.com>
2022-01-02 19:35:31 +0800

    add some function

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Splashscreen extends StatelessWidget {
   const Splashscreen({Key? key}) : super(key: key);
@@ -7,18 +8,35 @@ class Splashscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 50,
-              width: 50,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/logo/main_logo.png'),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 50,
+            left: 30,
+          ),
+          child: Column(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo/main_logo.png'),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Find Cozy House\nto Stay and Happy',
+                style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                )),
+              )
+            ],
+          ),
         ),
       ),
     );
